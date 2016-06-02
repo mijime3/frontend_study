c9828681e1600fa5db4f847870a7188f11176ca9
Hajime IGARI <orkafriday@gmail.com>
2016-06-03 02:15:14 +0900

    backbone.js collection

--- backbone/js/app.js
+++ backbone/js/app.js
@@ -16,12 +16,6 @@
 
     var TaskView = Backbone.View.extend({
         tagName: 'li',
-        events: {
-            "click .command": "sayHello"
-        },
-        sayHello: function() {
-            alert('hello!');
-        },
         template: _.template($('#task-template').html()),
         render: function() {
             var template = this.template(this.model.toJSON());
@@ -29,8 +23,38 @@
             return this;
         }
     });
-    var taskView = new TaskView({model: task});
-    console.log(taskView.render().el);
-    $('body').append(taskView.render().el);
+
+    // Collection
+
+    var Tasks = Backbone.Collection.extend({
+        model: Task
+    });
+
+    var TasksView = Backbone.View.extend({
+        tagName: 'ul',
+        render: function() {
+            this.collection.each(function(task){
+                var taskView = new TaskView({model: task});
+                this.$el.append(taskView.render().el);
+            }, this);
+
+            return this;
+        }
+    });
+    var tasks = new Tasks([
+        {
+            title: 'task1',
+            completed: true
+        },
+        {
+            title: 'task2'
+        },
+        {
+            title: 'task3'
+        }
+    ]);
+    console.log(tasks.toJSON());
+    var tasksView = new TasksView({collection: tasks});
+    $('#tasks').html(tasksView.render().el);
 
 })();
